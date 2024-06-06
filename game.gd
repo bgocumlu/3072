9185102107757a6543f1469176469a1547995082
berkay-g <35933635+berkay-g@users.noreply.github.com>
2024-06-07 00:45:05 +0300

    Update game.gd

--- game.gd
+++ game.gd
@@ -20,7 +20,7 @@ var best_score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Engine.max_fps = 60
+	Engine.max_fps = 144
 	reset()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
